2c8017a52ccb8d5ca8c902798c86558270a84c62
qixiao <651901286@qq.com>
2023-08-08 17:29:09 +0800

    状态管理

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,14 @@
+<script setup>
+const counter = useState('counter', () => Math.round(Math.random() * 1000))
+</script>
 <template>
   <div>
-    测试页面
+    Counter: {{ counter }}
+    <button @click="counter++">
+      +
+    </button>
+    <button @click="counter--">
+      -
+    </button>
   </div>
-  <button @click="btn()">请求测试按钮</button>
 </template>
-
-<script setup lang="ts">
-//引入http.ts封装的网络请求方法
-import { http } from '~/composables/http';
-
-const btn = async () => {
-  //定义请求参数
-  const obj = {
-    method: "POST",
-    url: '/user/login',
-    //"POST"方法传参:
-    body: {
-      name: "测试",
-      phone: "176xxxxxx1",
-      password: "test123",
-    },
-    //"GET"方法URL传参:
-    // query: {
-    //     name:"test"
-    // }
-  }
-  //开始请求
-  let res = await http(obj)
-  console.log("请求结果：", res);
-}
-
-</script>
-
