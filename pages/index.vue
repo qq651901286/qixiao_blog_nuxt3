d9d99798fd98755fecb930e57c5fd6bcc3c6cc45
qixiao <651901286@qq.com>
2023-08-08 17:27:15 +0800

    网络请求

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
-    <h1>Home page</h1>
-    <NuxtLink to="/about">About page</NuxtLink>
+    测试页面
   </div>
+  <button @click="btn()">请求测试按钮</button>
 </template>
 
 <script setup lang="ts">
-definePageMeta({
-  pageTransition: {
-    name: 'rotate'
+//引入http.ts封装的网络请求方法
+import { http } from '~/composables/http';
+
+const btn = async () => {
+  //定义请求参数
+  const obj = {
+    method: "POST",
+    url: '/user/login',
+    //"POST"方法传参:
+    body: {
+      name: "测试",
+      phone: "176xxxxxx1",
+      password: "test123",
+    },
+    //"GET"方法URL传参:
+    // query: {
+    //     name:"test"
+    // }
   }
-})
+  //开始请求
+  let res = await http(obj)
+  console.log("请求结果：", res);
+}
+
 </script>
+
