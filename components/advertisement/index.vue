5e50ba0bbea93086236e6704e2a6ff04f88a11b0
qixiao <651901286@qq.com>
2023-08-10 11:07:16 +0800

    轮播图开发

--- components/advertisement/index.vue
+++ components/advertisement/index.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="content">
-
+    <el-carousel :interval="5000" type="card" height="300px">
+      <el-carousel-item>
+        <img src="~/assets/img/ad/1.jpeg" alt="">
+      </el-carousel-item>
+      <el-carousel-item>
+        <img src="~/assets/img/ad/2.jpeg" alt="">
+      </el-carousel-item>
+      <el-carousel-item>
+        <img src="~/assets/img/ad/3.jpg" alt="">
+      </el-carousel-item>
+      <el-carousel-item>
+        <img src="~/assets/img/ad/4.jpg" alt="">
+      </el-carousel-item>
+      <el-carousel-item>
+        <img src="~/assets/img/ad/5.jpg" alt="">
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
 <script setup lang="ts">
+
 
 </script>
 
@@ -12,7 +29,11 @@
 .content {
   width: 100%;
   height: 300px;
-  background: #d4d4d4;
+
+  img {
+    width: 100%;
+    height: 300px;
+  }
 }
 
 </style>
