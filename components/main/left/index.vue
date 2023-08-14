3c406f6c9bad53fb28f776e8354df77da327d0bc
qixiao <651901286@qq.com>
2023-08-14 18:27:18 +0800

    中间

--- components/main/left/index.vue
+++ components/main/left/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container">
-    <div class="up">
+    <div class="new-blog">
       <!--最新博客-->
-      <div class="new-blog">
+      <div class="up">
         <span class="title">
           最新博客
         </span>
         <span class="more">
-          <el-link href="https://element-plus.org" target="_blank">
+          <el-link default-expand-all href="https://element-plus.org" target="_blank">
             更多
           </el-link>
         </span>
@@ -20,8 +20,22 @@
         </ul>
       </div>
     </div>
-    <div class="centre">
-      中
+    <div class="category">
+      <!--分类-->
+      <div class="up">
+        <span class="title">
+          分类
+        </span>
+        <span class="more">
+          <el-link href="https://element-plus.org" target="_blank">
+            更多
+          </el-link>
+        </span>
+      </div>
+      <!--内容区域-->
+      <div class="content">
+        <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick" />
+      </div>
     </div>
     <div class="below">
       下
@@ -34,6 +48,100 @@ const blogClick = (id:number)=> {
   console.log(id);
 }
 
+
+interface Tree {
+  label: string
+  children?: Tree[]
+}
+
+const handleNodeClick = (data: Tree) => {
+  console.log(data)
+}
+
+const data: Tree[] = [
+  {
+    label: '编程',
+    children: [
+      {
+        label: '前端',
+        children: [
+          {
+            label: 'Vue',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: 'Level one 2',
+    children: [
+      {
+        label: 'Level two 2-1',
+        children: [
+          {
+            label: 'Level three 2-1-1',
+          },
+        ],
+      },
+      {
+        label: 'Level two 2-2',
+        children: [
+          {
+            label: 'Level three 2-2-1',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: 'Level one 3',
+    children: [
+      {
+        label: 'Level two 3-1',
+        children: [
+          {
+            label: 'Level three 3-1-1',
+          },
+        ],
+      },
+      {
+        label: 'Level two 3-2',
+        children: [
+          {
+            label: 'Level three 3-2-1',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: 'Level one 3',
+    children: [
+      {
+        label: 'Level two 3-1',
+        children: [
+          {
+            label: 'Level three 3-1-1',
+          },
+        ],
+      },
+      {
+        label: 'Level two 3-2',
+        children: [
+          {
+            label: 'Level three 3-2-1',
+          },
+        ],
+      },
+    ],
+  },
+]
+
+const defaultProps = {
+  children: 'children',
+  label: 'label',
+}
+
 </script>
 
 <style scoped lang="scss">
@@ -41,12 +149,12 @@ const blogClick = (id:number)=> {
   display: flex;
   flex-direction: column;
   width: 100%;
-  .up {
+  .new-blog {
     background: beige;
     height: 300px;
     width: 100%;
 
-    .new-blog {
+    .up {
       width: 100%;
       height: 40px;
       display: flex;
@@ -91,11 +199,33 @@ const blogClick = (id:number)=> {
       }
     }
   }
-  .centre {
+  .category {
     margin-top: 20px;
     height: 560px;
     width: 100%;
     background: rgba(251, 204, 212, 0.89);
+
+    .up {
+      width: 100%;
+      height: 40px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      .title {
+        margin-left: 10px;
+      }
+      .more {
+        margin-right: 10px;
+        ::v-deep(.el-link__inner){
+          font-size: 16px;
+        }
+      }
+    }
+
+    .content {
+      display: flex;
+    }
   }
   .below {
     margin-top: 20px;
