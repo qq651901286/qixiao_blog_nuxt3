093c30b2147df11325673de335070208697db724
qixiao <651901286@qq.com>
2023-08-23 16:57:40 +0800

    分类

--- pages/index.vue
+++ pages/index.vue
@@ -6,13 +6,13 @@
         <div class="left">
           <el-carousel height="500px" :interval="5000">
             <el-carousel-item>
-              <el-image src="https://china-qixiao.oss-cn-beijing.aliyuncs.com/qixiao-blog/ad/5.jpg" alt="" />
+              <el-image src="https://china-qixiao.oss-cn-beijing.aliyuncs.com/qixiao-blog/ad/5.jpg" alt=""/>
             </el-carousel-item>
             <el-carousel-item>
-              <el-image src="https://china-qixiao.oss-cn-beijing.aliyuncs.com/qixiao-blog/ad/4.jpg" alt="" />
+              <el-image src="https://china-qixiao.oss-cn-beijing.aliyuncs.com/qixiao-blog/ad/4.jpg" alt=""/>
             </el-carousel-item>
             <el-carousel-item>
-              <el-image src="https://china-qixiao.oss-cn-beijing.aliyuncs.com/qixiao-blog/ad/3.jpg" alt="" />
+              <el-image src="https://china-qixiao.oss-cn-beijing.aliyuncs.com/qixiao-blog/ad/3.jpg" alt=""/>
             </el-carousel-item>
           </el-carousel>
         </div>
@@ -29,10 +29,15 @@
       <!--分类-->
       <div class="category">
         <el-card shadow="never">
-          <el-space wrap>
-            <el-link :underline="false" v-for="i in 100">后端</el-link>
-          </el-space>
-
+          <el-tabs tab-position="left" @tab-click="categoryClick">
+            <el-tab-pane :key="item.id" :label="item.name" v-for="item in categoryList">
+              <el-space wrap>
+                <el-check-tag v-for="tag in item.tagList" :checked="tag.isChoice" @change="tagClick(tag)">
+                  {{ tag.name  }}
+                </el-check-tag>
+              </el-space>
+            </el-tab-pane>
+          </el-tabs>
         </el-card>
       </div>
     </div>
@@ -43,14 +48,162 @@
 definePageMeta({
   title: '首页'
 })
-import { useSettingStore } from '@/stores/setting'
-import { useCounterStore } from '@/stores/counter'
+import {useSettingStore} from '@/stores/setting'
+import {useCounterStore} from '@/stores/counter'
+
 const counter = useCounterStore()
 const setting = useSettingStore()
-const btnClick = ()=> {
+const btnClick = () => {
   counter.increment()
 }
 
+interface Tag {
+  // id：'1'
+  id: string,
+  // 标题：python,java
+  name: string,
+  // 选择状态
+  isChoice:false,
+}
+
+interface Category {
+  // id：'1'
+  id: string,
+  // 名称：后端
+  name: string,
+  // 标签
+  tagList: Tag[],
+}
+const checked = ref(false)
+const onChange = (status: boolean) => {
+  checked.value = status
+}
+
+// 分类数据
+const categoryList:Category[] = ref<Category[]>();
+
+//分类被点击
+const categoryClick = (pane,ev)=> {
+  // 从0开始，pane.index就是第几个
+  // 获取元素内容
+  let item = categoryList.value[pane.index]
+  // 刷新页面
+}
+
+// 标签被点击
+const tagClick = (tag:Tag)=> {
+  tag.isChoice = !tag.isChoice
+
+  // 刷新列表
+}
+
+onMounted(() => {
+  categoryList.value = [
+    {
+      id:'1',
+      name:'编程语言',
+      tagList:[
+        {
+          id:'1',
+          name:'Java',
+          isChoice:false,
+        },
+        {
+          id:'2',
+          name:'Python',
+          isChoice:false,
+        },
+        {
+          id:'5',
+          name:'Python',
+          isChoice:false,
+        },
+        {
+          id:'3',
+          name:'PHP',
+          isChoice:false,
+        },
+        {
+          id:'4',
+          name:'C#',
+          isChoice:false,
+        },
+      ]
+    },
+    {
+      id:'2',
+      name:'Java',
+      tagList:[
+        {
+          id:'1',
+          name:'java',
+          isChoice:false,
+        },
+        {
+          id:'2',
+          name:'tomcat',
+          isChoice:false,
+        },
+        {
+          id:'3',
+          name:'jvm',
+          isChoice:false,
+        },
+        {
+          id:'4',
+          name:'StringBoot',
+          isChoice:false,
+        },
+        {
+          id:'5',
+          name:'StringCloud',
+          isChoice:false,
+        },
+        {
+          id:'6',
+          name:'String',
+          isChoice:false,
+        },
+      ]
+    },
+    {
+      id:'3',
+      name:'Unity3D',
+      tagList:[
+        {
+          id:'1',
+          name:'建模',
+          isChoice:false,
+        },
+        {
+          id:'2',
+          name:'材质',
+          isChoice:false,
+        },
+        {
+          id:'3',
+          name:'C#',
+          isChoice:false,
+        },
+        {
+          id:'4',
+          name:'游戏开发',
+          isChoice:false,
+        },
+        {
+          id:'5',
+          name:'王者荣耀',
+          isChoice:false,
+        },
+        {
+          id:'6',
+          name:'3D游戏',
+          isChoice:false,
+        },
+      ]
+    },
+  ]
+})
 </script>
 
 <style scoped lang="scss">
@@ -90,8 +243,6 @@ const btnClick = ()=> {
 
     .category {
       .el-card {
-        .el-link {
-        }
       }
     }
   }
