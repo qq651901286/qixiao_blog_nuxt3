2058d9e3b0eae2ad2bb83c33db4f82062a699227
qixiao <651901286@qq.com>
2023-08-28 11:47:17 +0800

    能跳转到首页，固定头部

--- components/header/index.vue
+++ components/header/index.vue
@@ -1,97 +1,95 @@
 <template>
   <div class="container">
-    <div class="content">
-      <div class="menu">
-        <el-menu
-            v-show="false"
-            :default-active="activeIndex"
-            class="el-menu-demo"
-            mode="horizontal"
-            :ellipsis="false"
-            @select="handleSelect"
-        >
-          <el-menu-item index="100">
-            <img src="https://china-qixiao.oss-cn-beijing.aliyuncs.com/qixiao-blog/logo.png"/>
-          </el-menu-item>
-          <el-menu-item index="200">首页</el-menu-item>
-          <el-menu-item index="300">博客</el-menu-item>
-          <el-menu-item index="400">留言板</el-menu-item>
-          <el-menu-item index="500">友链</el-menu-item>
-          <el-menu-item index="600">关于</el-menu-item>
-          <el-sub-menu index="700">
-            <template #title>自己</template>
-            <el-menu-item index="700-1">Gitee</el-menu-item>
-            <el-menu-item index="700-2">Github</el-menu-item>
-            <el-menu-item index="700-3">个人经历</el-menu-item>
-            <el-menu-item index="700-4">联系方式</el-menu-item>
-            <el-sub-menu index="700-5">
-              <template #title>系统推荐</template>
-              <el-menu-item index="700-5-1">个人博客</el-menu-item>
-              <el-menu-item index="700-5-2">相亲阁</el-menu-item>
-              <el-menu-item index="700-5-3">投资分析系统</el-menu-item>
+    <el-affix>
+      <div class="content">
+        <div class="menu">
+          <el-menu
+              v-show="false"
+              class="el-menu-demo"
+              mode="horizontal"
+              :ellipsis="false"
+              @select="handleSelect"
+          >
+            <el-menu-item index="100">
+              <img src="https://china-qixiao.oss-cn-beijing.aliyuncs.com/qixiao-blog/logo.png"/>
+            </el-menu-item>
+            <el-menu-item index="200">首页</el-menu-item>
+            <el-menu-item index="300">博客</el-menu-item>
+            <el-menu-item index="400">留言板</el-menu-item>
+            <el-menu-item index="500">友链</el-menu-item>
+            <el-menu-item index="600">关于</el-menu-item>
+            <el-sub-menu index="700">
+              <template #title>自己</template>
+              <el-menu-item index="700-1">Gitee</el-menu-item>
+              <el-menu-item index="700-2">Github</el-menu-item>
+              <el-menu-item index="700-3">个人经历</el-menu-item>
+              <el-menu-item index="700-4">联系方式</el-menu-item>
+              <el-sub-menu index="700-5">
+                <template #title>系统推荐</template>
+                <el-menu-item index="700-5-1">个人博客</el-menu-item>
+                <el-menu-item index="700-5-2">相亲阁</el-menu-item>
+                <el-menu-item index="700-5-3">投资分析系统</el-menu-item>
+              </el-sub-menu>
             </el-sub-menu>
-          </el-sub-menu>
-        </el-menu>
-        <el-menu
-            :default-active="activeIndex"
-            class="el-menu-demo"
-            mode="horizontal"
-            :ellipsis="false"
-            @select="handleSelect"
-        >
-          <el-menu-item index="100">
-            <img src="https://china-qixiao.oss-cn-beijing.aliyuncs.com/qixiao-blog/logo.png"/>
-          </el-menu-item>
-          <el-menu-item index="200">首页</el-menu-item>
-          <el-menu-item index="300">博客</el-menu-item>
-          <el-menu-item index="400">留言板</el-menu-item>
-          <el-menu-item index="500">友链</el-menu-item>
-          <el-menu-item index="600">关于</el-menu-item>
-          <el-sub-menu index="700">
-            <template #title>自己</template>
-            <el-menu-item index="700-1">Gitee</el-menu-item>
-            <el-menu-item index="700-2">Github</el-menu-item>
-            <el-menu-item index="700-3">个人经历</el-menu-item>
-            <el-menu-item index="700-4">联系方式</el-menu-item>
-            <el-sub-menu index="700-5">
-              <template #title>系统推荐</template>
-              <el-menu-item index="700-5-1">个人博客</el-menu-item>
-              <el-menu-item index="700-5-2">相亲阁</el-menu-item>
-              <el-menu-item index="700-5-3">投资分析系统</el-menu-item>
+          </el-menu>
+          <el-menu
+              class="el-menu-demo"
+              mode="horizontal"
+              :ellipsis="false"
+              @select="menuHandleSelect"
+          >
+            <el-menu-item index="100">
+              <img src="https://china-qixiao.oss-cn-beijing.aliyuncs.com/qixiao-blog/logo.png"/>
+            </el-menu-item>
+            <el-menu-item index="200">首页</el-menu-item>
+            <!--<el-menu-item index="300">博客</el-menu-item>-->
+            <el-menu-item index="400">留言板</el-menu-item>
+            <el-menu-item index="500">友链</el-menu-item>
+            <el-menu-item index="600">关于</el-menu-item>
+            <el-sub-menu index="700">
+              <template #title>自己</template>
+              <el-menu-item index="700-1">Gitee</el-menu-item>
+              <el-menu-item index="700-2">Github</el-menu-item>
+              <el-menu-item index="700-3">个人经历</el-menu-item>
+              <el-menu-item index="700-4">联系方式</el-menu-item>
+              <el-sub-menu index="700-5">
+                <template #title>系统推荐</template>
+                <el-menu-item index="700-5-1">个人博客</el-menu-item>
+                <el-menu-item index="700-5-2">相亲阁</el-menu-item>
+                <el-menu-item index="700-5-3">投资分析系统</el-menu-item>
+              </el-sub-menu>
             </el-sub-menu>
-          </el-sub-menu>
-        </el-menu>
+          </el-menu>
+        </div>
+        <div class="search">
+          <el-autocomplete
+              v-model="searchContent"
+              :fetch-suggestions="querySearchAsync"
+              placeholder="请输入内容"
+              @select="searchSelect"
+              clearable
+          ></el-autocomplete>
+        </div>
       </div>
-      <div class="search">
-        <el-autocomplete
-            v-model="searchContent"
-            :fetch-suggestions="querySearchAsync"
-            placeholder="请输入内容"
-            @select="searchSelect"
-            clearable
-        ></el-autocomplete>
-      </div>
-    </div>
+    </el-affix>
   </div>
 </template>
 
 <script setup lang="ts">
-import {useSettingStore} from "~/stores/setting";
-
-const settingStore = useSettingStore()
-const hoverColor = ref<string>(settingStore.hoverColor);
-
 // 路由
 const router = useRouter()
 
+// 菜单被点击
 const menuHandleSelect = (index: string, pathList: string[]) => {
-  console.log('menuHandleSelect被单击')
-}
+  if ('100' === index || '200' === index){
+    // log 首页被点击
+    // 跳到首页
+    router.push('/')
+  }
 
-const menuItemClick = () => {
-  console.log('menuItemClick被单击')
+  console.log(index)
+  console.log(pathList)
 }
-
 
 interface LinkItem {
   value: string
@@ -155,6 +153,7 @@ onMounted(() => {
   display: flex;
   justify-content: center;
   .content{
+    background: #ffffff;
     width: 1200px;
     height: 60px;
     display: flex;
