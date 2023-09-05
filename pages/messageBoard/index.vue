5a4a83a86085b14bd490644867b0612421c86a41
qixiao <651901286@qq.com>
2023-09-05 23:09:49 +0800

    留言板demo

---
+++ pages/messageBoard/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="content">
+      留言板
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  justify-content: center;
+  .content{
+    width: 1200px;
+  }
+}
+</style>
