68ff32f4dc87b911827cd0bc00402ab749d50851
qixiao <651901286@qq.com>
2023-09-06 13:48:44 +0800

    留言板添加弹幕

--- pages/messageBoard/index.vue
+++ pages/messageBoard/index.vue
@@ -1,12 +1,65 @@
 <template>
   <div class="container">
     <div class="content">
-      留言板
+      <client-only>
+        <vue-danmaku ref="danmaku" v-model:danmus="danmus" useSlot loop>
+          <template v-slot:dm="{ index, danmu }">
+            <span class="text">{{ index }}{{ danmu.name }}：{{ danmu.text }}</span>
+          </template>
+        </vue-danmaku>
+      </client-only>
+
+      <div class="send">
+
+      </div>
+
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import vueDanmaku from 'vue3-danmaku'
+
+const danmus = ref([
+  { avatar: 'http://a.com/a.jpg', name: 'a', text: 'aaa' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+  { avatar: 'http://a.com/b.jpg', name: 'b', text: 'bbb' },
+])
+
 
 </script>
 
@@ -16,6 +69,20 @@
   justify-content: center;
   .content{
     width: 1200px;
+    height: 800px;
+
+    .vue-danmaku {
+      background: #332c2c;
+      height: 300px;
+      .text {
+        color: #d37c8e;
+      }
+    }
+    .send{
+      height: 100px;
+      margin-top: 20px;
+      background: #2d8cf0;
+    }
   }
 }
 </style>
