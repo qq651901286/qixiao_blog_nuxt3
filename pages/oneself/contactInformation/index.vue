93c41f819005dbd9ddea078c663685009f769fe1
qixiao <651901286@qq.com>
2023-09-07 21:15:03 +0800

    联系方式  开发

--- pages/oneself/contactInformation/index.vue
+++ pages/oneself/contactInformation/index.vue
@@ -1,13 +1,33 @@
 <script setup lang="ts">
+import { MdPreview } from 'md-editor-v3';
+import 'md-editor-v3/lib/preview.css';
 
+
+const id = 'preview-only';
+const text = ref('' +
+    '# 联系方式\n' +
+    '## QQ\n' +
+    '651901286\n' +
+    '## Email\n' +
+    '651901286@qq.com\n'
+)
 </script>
 
 <template>
-  <div>
-    联系方式
+  <div class="container">
+    <div class="content">
+      <MdPreview :editorId="id" :modelValue="text" />
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-
+.container {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  .content{
+    width: 1200px;
+  }
+}
 </style>
