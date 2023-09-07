cd70449b2bb9e45ddcbc075b8864c343321c1770
qixiao <651901286@qq.com>
2023-09-08 01:04:54 +0800

    优化页面效果

--- components/footer/index.vue
+++ components/footer/index.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="container">
     <div class="content">
-      <div class="left">
-        <div>
-          <el-link :underline="false" href="https://gitee.com/qq651901286" target="_blank">
-            Crafted with by Qixiao
-          </el-link>
-        </div>
-        <div>
-          <el-link :underline="false" href="https://gitee.com/qq651901286" target="_blank">
-            粤ICP备XXXX号
-          </el-link>
-        </div>
-      </div>
-      <div class="right">
-        <div>
-          <el-link disabled>访问量：999</el-link>
-        </div>
-        <div>
-          <el-link disabled>运行天数：388</el-link>
-        </div>
-      </div>
+      <el-card shadow="hover">
+        <el-row justify="space-between" align="middle">
+          <el-col :span="6">
+            <el-link :underline="false" href="https://gitee.com/qq651901286" target="_blank">
+              Crafted with by Qixiao
+            </el-link>
+          </el-col>
+          <el-col :span="4">
+            <el-link :underline="false" href="https://gitee.com/qq651901286" target="_blank">
+              粤ICP备XXXX号
+            </el-link>
+          </el-col>
+
+          <el-col :span="4">
+            <el-statistic group-separator="" title="访问量" :value="268500"/>
+          </el-col>
+          <el-col :span="1">
+            <el-statistic group-separator="" title="运行天数" :value="3300"/>
+          </el-col>
+        </el-row>
+      </el-card>
+
     </div>
 
   </div>
@@ -36,12 +38,19 @@ const handleSelect = (key: string, keyPath: string[]) => {
 .container {
   display: flex;
   justify-content: center;
+
   .content {
+    margin-top: 20px;
+    margin-bottom: 20px;
     height: 60px;
     width: 1200px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+
+    .el-card {
+      .el-link {
+        font-size: 20px;
+      }
+    }
+
   }
 }
 
